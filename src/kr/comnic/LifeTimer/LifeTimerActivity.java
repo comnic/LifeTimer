913c2b273858d941436a0e36bb73f5328ac4f756
comnic_home <comnic@hanmail.net>
2011-06-13 00:31:08 +0900

    주석 추가.
    Change-Id: Ie6523db3a6fdb787f899a6e4ac527deb80a758fd

--- src/kr/comnic/LifeTimer/LifeTimerActivity.java
+++ src/kr/comnic/LifeTimer/LifeTimerActivity.java
@@ -14,5 +14,6 @@ public class LifeTimerActivity extends Activity {
         super.onCreate(savedInstanceState);
         //새로만든 View를 띄운다.
         setContentView(new TimeDrawView(this));
+        //앱에서 카울리 광고를 띄울려면, 상단부분을 80px정도 비워두고 만드는 것이 좋다.
     }
 }
